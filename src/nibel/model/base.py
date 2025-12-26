77696feb6bed7a7c9521001e065fe7f5387a7040
Georgy Pliev <howluwqz1@gmail.com>
2025-12-27 02:53:00 +0300

    fix: remove redudant init_subclass from Model

--- src/nibel/model/base.py
+++ src/nibel/model/base.py
@@ -50,16 +50,6 @@ def field(**kwargs: typing.Any) -> typing.Any:
 class Model(msgspec.Struct, **MODEL_CONFIG):
     if not typing.TYPE_CHECKING:
 
-        def __init_subclass__(cls, *args: typing.Any, **kwargs: typing.Any) -> None:
-            from telegrinder.tools.member_descriptor_proxy import MemberDescriptorProxy
-
-            result = super().__init_subclass__(*args, **kwargs)
-
-            for field_name in getattr(cls, "__slots__", ()):
-                setattr(cls, field_name, MemberDescriptorProxy(getattr(cls, field_name)))
-
-            return result
-
         def __getattribute__(self, name: str, /) -> typing.Any:
             class_ = type(self)
             val = object.__getattribute__(self, name)
