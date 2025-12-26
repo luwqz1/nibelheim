841004030549bec3150dff3b82c26b25d2790168
Georgy Pliev <howluwqz1@gmail.com>
2025-12-27 02:58:15 +0300

    fix import

--- src/nibel/webhook/__init__.py
+++ src/nibel/webhook/__init__.py
@@ -2,13 +2,13 @@ from nibel.webhook.dispatcher.abc import ABCDispatcher
 from nibel.webhook.dispatcher.dispatcher import Dispatcher
 from nibel.webhook.receiver.abc import ABCReceiver
 from nibel.webhook.receiver.receiver import Receiver
-from nibel.webhook.validation import validate_webhook_headers, validate_webhook_signature
+from nibel.webhook.validation import validate_webhook_headers, validate_webhook_signature_from_headers
 
 __all__ = (
     "ABCDispatcher",
     "ABCReceiver",
     "Dispatcher",
     "Receiver",
-    "validate_webhook_headers",
     "validate_webhook_signature",
+    "validate_webhook_signature_from_headers",
 )
